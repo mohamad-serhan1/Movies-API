dfeeea49a97e009d4fa11c74f702221716d2aede
mohammad-serhan1 <mohd.ser19@gmail.com>
2024-01-13 13:05:39 +0200

    adh

--- app/Movies/page.tsx
+++ app/Movies/page.tsx
@@ -10,10 +10,10 @@ import Movie from "@/components/movie";
 
 
   return (
-    <main className="p-24">
+    <main className="p-12 sm:p-24">
         <h1 className="font-bold text-2xl pb-9">Popular Movies</h1>
 
-      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-9">
         {response.results.map((movie: any) => (
           <Movie
             key={movie.id}
